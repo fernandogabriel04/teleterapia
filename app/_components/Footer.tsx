bf69100a1871930d52f96be5d3cc1bf431360753
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-02-11 15:50:09 -0300

    style: social medias commented

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -5,19 +5,19 @@ const Footer = () => {
           <p className="text-lg opacity-80 mb-4">
             Teleterapia Gratuita - Apoiando sua saúde mental, sem custos e com total privacidade.
           </p>
-          <div className="flex justify-center space-x-6 mb-6">
+          {/* <div className="flex justify-center space-x-6 mb-6">
             <a href="https://www.facebook.com/" target="_blank" className="hover:text-gray-200 transition duration-300">
               <i className="fab fa-facebook-f text-2xl"></i>
             </a>
             <a href="https://www.instagram.com/" target="_blank" className="hover:text-gray-200 transition duration-300">
               <i className="fab fa-instagram text-2xl"></i>
             </a>
-            <a href="https://twitter.com/" target="_blank" className="hover:text-gray-200 transition duration-300">
-              <i className="fab fa-twitter text-2xl"></i>
+            <a href="https://x.com/" target="_blank" className="hover:text-gray-200 transition duration-300">
+              <i className="fab fa-x text-2xl"></i>
             </a>
-          </div>
+          </div> */}
           <p className="text-sm opacity-60">
-            &copy; 2025 Teleterapia Gratuita. Todos os direitos reservados.
+            &copy; 2025 Teleterapia. Todos os direitos reservados.
           </p>
         </div>
       </footer>
