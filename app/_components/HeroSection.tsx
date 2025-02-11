fab80d7b8fd8913ab5dce6bf76eb45cc073c45fa
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-02-11 15:51:06 -0300

    fix: text, animations and style adjustments

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -14,27 +14,31 @@ const HeroSection = () => {
             Teleterapia <br /> Gratuita
           </h1>
           <p className="text-lg md:text-2xl mt-4 mb-8 opacity-80">
-            Você se surpreenderá. Fale com a gente.
+            Não silencie suas emoções. Fale com a gente.
           </p>
           <Link
             href="https://wa.me/33125492"
             target="_blank"
-            className="bg-white text-blue-500 font-semibold py-4 px-8 rounded-full text-lg shadow-lg hover:bg-gray-200 transition duration-300"
+            className="bg-white text-blue-500 font-semibold py-4 px-8 rounded-full text-lg shadow-lg hover:bg-gray-200 transition duration-300 transform hover:scale-105 hover:shadow-2xl"
           >
             Agende sua Sessão no WhatsApp
           </Link>
         </div>
 
-        {/* Imagem */}
-        <div className="w-full md:w-1/2 flex justify-center bg-[radial-gradient(circle_at_50%_50%,#fff,transparent_35%)]">
-          <Image
-            src="/owl-herosection.png"
-            alt="Coruja representando a teleterapia gratuita"
-            width={500} // Define tamanho otimizado
-            height={500}
-            priority // Para carregamento otimizado
-            className="w-auto h-auto max-w-xs md:max-w-sm"
-          />
+        {/* Imagem da coruja (visível apenas em telas maiores que 1024px) */}
+        <div className="w-full md:w-1/2 justify-end hidden lg:flex">
+          <div className="relative group mx-auto">
+            {/* Adicionando um efeito de sombra suave na imagem ao passar o mouse */}
+            <div className="absolute inset-0 opacity-30 rounded-lg group-hover:opacity-0 transition-opacity duration-300 bg-[radial-gradient(circle_at_50%_50%,#fff,transparent_50%)] z-0"></div>
+            <Image
+              src="/owl-herosection.png"
+              alt="Coruja representando a teleterapia gratuita"
+              width={500} // Define tamanho otimizado
+              height={500}
+              priority // Para carregamento otimizado
+              className="w-auto h-auto max-w-xs md:max-w-sm transition-transform duration-300 group-hover:scale-110"
+            />
+          </div>
         </div>
 
         {/* Div de Parceiros no canto inferior direito */}
@@ -45,6 +49,7 @@ const HeroSection = () => {
             width={300}
             height={80}
             priority
+            className="w-auto h-auto max-w-xs md:max-w-sm z-10"
           />
         </div>
       </div>
