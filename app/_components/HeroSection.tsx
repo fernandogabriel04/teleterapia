ecce876f2d91048103ee65796cc79e471ffe4a95
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-02-12 12:49:29 -0300

    feat: applying responsivity in hero section

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -5,12 +5,12 @@ const HeroSection = () => {
   return (
     <section
       id="hero"
-      className="relative flex flex-col md:flex-row justify-center items-center min-h-screen text-center bg-blue-500 text-white px-6 pt-24"
+      className="relative flex flex-col md:flex-row justify-start items-center md:min-h-screen text-center bg-blue-500 text-white px-6 pt-24 mt-8 md:mt-0"
     >
-      <div className="flex flex-col md:flex-row max-w-6xl mx-auto items-center justify-between w-full space-between">
+      <div className="flex flex-col md:flex-row max-w-6xl mx-auto items-center justify-between w-full space-between h-auto">
         {/* Texto */}
         <div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left">
-          <h1 className="text-5xl md:text-7xl font-extrabold leading-tight uppercase">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold leading-tight uppercase">
             Teleterapia <br /> Gratuita
           </h1>
           <p className="text-lg md:text-2xl mt-4 mb-8 opacity-80">
@@ -25,31 +25,31 @@ const HeroSection = () => {
           </Link>
         </div>
 
-        {/* Imagem da coruja (visível apenas em telas maiores que 1024px) */}
-        <div className="w-full md:w-1/2 justify-end hidden lg:flex">
+        {/* Imagem da coruja */}
+        <div className="w-full flex lg:w-1/2 justify-center md:justify-end z-20 mb-[4.5rem]">
           <div className="relative group mx-auto">
             {/* Adicionando um efeito de sombra suave na imagem ao passar o mouse */}
             <div className="absolute inset-0 opacity-30 rounded-lg group-hover:opacity-0 transition-opacity duration-300 bg-[radial-gradient(circle_at_50%_50%,#fff,transparent_50%)] z-0"></div>
             <Image
               src="/owl-herosection.png"
               alt="Coruja representando a teleterapia gratuita"
-              width={500} // Define tamanho otimizado
+              width={500}
               height={500}
-              priority // Para carregamento otimizado
-              className="w-auto h-auto max-w-xs md:max-w-sm transition-transform duration-300 group-hover:scale-110"
+              priority
+              className="relative w-auto h-auto max-w-64 md:max-w-sm transition-transform duration-300 group-hover:scale-110"
             />
           </div>
         </div>
 
-        {/* Div de Parceiros no canto inferior direito */}
-        <div className="absolute bottom-0 right-0 min-w-[400px] h-[100px] bg-gray-50 rounded-tl-[20px] z-10 flex items-center justify-center">
+        {/* Div de Parceiros no canto inferior direito, ocupando toda a largura */}
+        <div className="absolute bottom-0 left-0 w-full h-[100px] bg-gray-50 rounded-none z-10 items-center justify-center flex">
           <Image
             src="/parceiros.png"
             alt="Parceiros"
             width={300}
             height={80}
             priority
-            className="w-auto h-auto max-w-xs md:max-w-sm z-10"
+            className="relative w-auto h-auto max-w-xs md:max-w-sm z-0"
           />
         </div>
       </div>
