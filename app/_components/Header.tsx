ae20c2058f24f09be99ebf83587d64e370573991
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-02-15 13:31:37 -0300

    style: modified header style

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white py-4 px-6 shadow-lg fixed top-0 left-0 right-0 z-50">
+    <header className="bg-white text-white py-4 px-6 shadow-lg fixed top-0 left-0 right-0 z-50">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         {/* Título à esquerda */}
         <div className="flex items-center">
@@ -17,31 +17,31 @@ const Header = () => {
         <nav className="hidden md:flex space-x-8">
           <Link
             href="#hero"
-            className="hover:text-gray-200 transition duration-300"
+            className="text-[#234C90] hover:text-blue-700 transition duration-300"
           >
             Início
           </Link>
           <Link
             href="#benefits"
-            className="hover:text-gray-200 transition duration-300"
+            className="text-[#234C90] hover:text-blue-700 transition duration-300"
           >
             Benefícios
           </Link>
           <Link
             href="#how-it-works"
-            className="hover:text-gray-200 transition duration-300"
+            className="text-[#234C90] hover:text-blue-700 transition duration-300"
           >
             Como Funciona
           </Link>
           <Link
             href="#feedbacks"
-            className="hover:text-gray-200 transition duration-300"
+            className="text-[#234C90] hover:text-blue-700 transition duration-300"
           >
             Feedbacks
           </Link>
           <Link
             href="#faq"
-            className="hover:text-gray-200 transition duration-300"
+            className="text-[#234C90] hover:text-blue-700 transition duration-300"
           >
             FAQ
           </Link>
@@ -52,7 +52,7 @@ const Header = () => {
           <Link
             href="https://wa.me/+558233125492"
             target="_blank"
-            className="flex items-center bg-white text-blue-500 font-semibold py-2 px-6 rounded-full shadow-lg hover:bg-gray-200 transition duration-300"
+            className="flex items-center bg-gradient-to-b from-[#3B82F6] to-[#234C90] text-[#EFF6FF] font-semibold py-2 px-6 rounded-full shadow-lg hover:bg-gray-200 transition duration-300"
           >
             <FaWhatsapp className="mr-2 text-lg" />
             Atendimento 24h
