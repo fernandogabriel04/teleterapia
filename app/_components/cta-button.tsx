234dfd55bd13f17ddc666dcff802342e3bd0fb88
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-02-15 13:31:24 -0300

    style: modified button style

--- app/_components/cta-button.tsx
+++ app/_components/cta-button.tsx
@@ -3,7 +3,7 @@ const CTAButton = () => {
         <a
             href="https://wa.me/+558233125492"
             target="_blank"
-            className="bg-white text-blue-500 font-semibold mt-6 py-4 px-8 rounded-full text-lg shadow-lg hover:bg-gray-200 transition duration-300 transform hover:scale-105 hover:shadow-md"
+            className="bg-gradient-to-b from-[#3B82F6] to-[#234C90] text-[#EFF6FF] font-semibold mt-6 py-4 px-8 rounded-full text-lg shadow-lg hover:bg-gray-200 transition duration-300 transform hover:scale-105 hover:shadow-md"
           >
             Faça sua Sessão Agora
           </a>
