576b31aeb8b169d343f9a678dbeec9d5f5eb1f3e
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2025-04-06 12:43:59 -0300

    feat: add VideoSection

---
+++ app/_components/VideoSection.tsx
@@ -0,0 +1,28 @@
+export default function VideoSection() {
+    return (
+        <section className="bg-zinc-100 text-zinc-900 pt-56 px-24 gap-4 flex flex-col items-center">
+            <div className="flex justify-between gap-16 items-center">
+                <h1 className="text-4xl text-end text-nowrap text-secondary-blue">24h por dia<br/>7 dias por semana.<br/><strong>Veja no vídeo<br/> como funciona</strong></h1>
+                    <div className="bg-primary-blue min-w-4 h-16 rounded-full"></div>
+                <p className="text-xl">Se você precisa de apoio emocional, o <strong>
+                    Teleterapia Maceió está
+                    aqui para ajudar. Atendimento psicológico gratuito, sigiloso e
+                    acessível para toda a população
+                </strong>. É um serviço inovador da Secretaria 
+                Municipal de Saúde que oferece atendimento psicológico gratuito para 
+                todos que precisam de suporte emocional.
+                </p>
+            </div>
+            <a href="#" className="text-primary-blue underline"><strong>Saiba mais +</strong></a>
+            <iframe
+                className="mt-8 w-full aspect-video rounded-3xl"
+                src="https://www.youtube.com/embed/ogDo-0zDiA0?si=fR5ke_xria2pOTtA"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            />
+        </section>
+    );
+}
