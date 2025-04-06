1f72b6853d2460a59e3f1205e77a4fd8c3d1e042
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-04-06 12:55:54 -0300

    style: adjust padding hero section and justify content in center footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -8,7 +8,7 @@ import iscb from "@/public/iscb.svg";
 export default function Footer() {
     return (
         <footer className="bg-zinc-300 px-14 flex flex-col items-center justify-center min-h-[304px]">
-            <div className="flex items-center justify-around w-full py-4 gap-4 md:gap-16 bg-zinc-100 rounded-3xl relative -top-36 shadow-xl px-4">
+            <div className="flex items-center justify-center w-full py-4 gap-4 md:gap-16 bg-zinc-100 rounded-3xl relative -top-36 shadow-xl px-4">
                 <div className="flex-1 max-w-[150px]">
                     <Image
                     src={footerTeleterapia}
